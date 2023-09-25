93a213e7c64af582d7fe58808fff971555a2a0b4
Rhandel <ianian10@gmail.com>
2023-09-25 23:05:55 +0800

    Photo_Gallery V.1.0

---
+++ src/config/firebase.config.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD98Sdro4w-ra2IOyxmUak5JlHIEbnx3hE",
+  authDomain: "pgstorage-ac1c6.firebaseapp.com",
+  projectId: "pgstorage-ac1c6",
+  storageBucket: "pgstorage-ac1c6.appspot.com",
+  messagingSenderId: "767287704063",
+  appId: "1:767287704063:web:595451b2e07006f135cfca",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
